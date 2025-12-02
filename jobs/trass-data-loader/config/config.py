2f3a8b15ce9d14a41a21d300dcd43f5a843104a7
federico-barreca <federico.barreca@menteru.jp>
2025-12-02 14:10:37 +0900

    changed DAta source type to remote

--- jobs/trass-data-loader/config/config.py
+++ jobs/trass-data-loader/config/config.py
@@ -6,9 +6,8 @@ class DataSourceType(str, Enum):
     REMOTE = "REMOTE"
 
 
-DATA_SOURCE_TYPE = DataSourceType.LOCAL
+DATA_SOURCE_TYPE = DataSourceType.REMOTE
 
-LOCAL_INPUT_DATA_PATH = "data/00_InputData"
 LOCAL_LOADED_DATA_PATH = "data/06_LoadedData"
 
 IDU_FILENAME_PREFIX = "idu_loaded"
