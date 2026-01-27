ab32bee4720618b11ba7826a5fbc38341f41e390
federico-barreca <federico.barreca@menteru.jp>
2026-01-27 16:15:15 +0900

    deploy test

--- jobs/trass-data-loader/config/config_gcp.py
+++ jobs/trass-data-loader/config/config_gcp.py
@@ -9,6 +9,7 @@ class GCPEnv:
 
     START_DATE = os.environ.get("START_DATE")
     END_DATE = os.environ.get("END_DATE")
+
     FACILITY_ID = os.environ.get("FACILITY_ID")
 
     MASTER_DATA_PATH = os.environ.get("MASTER_DATA_PATH") or "01_MasterData"
